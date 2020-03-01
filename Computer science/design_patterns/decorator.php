b8d1480618241149a39eb94c81041430a1b26d9d
srimuthurajesh <srimuthurajesh@gmail.com>
2020-03-01 21:14:28 +0530

    Delete decorator.php

--- Computer science/design_patterns/decorator.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-#Behavioural design pattern
-#Alternative for inheritance (dynamic inheritance)
-#this happens after creating builder or base object
-#it does not affect the base object, but added feature to baseObject
